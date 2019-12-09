57ee3e159dd1d3929dbc0442bd46e8f3686080b9
Sharanagouda <Sharanu.mdl@gmail.com>
2019-12-09 16:41:26 +0530

    redux connected

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Main from "./main";
-import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./config/Store";
+
+const store = configureStore();
 
 function App() {
   return (
-    <div className="App">
-      <Main/>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+          <Main/>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
