9db907e508b6714b2c55b57ccbff301fa9f12484
Sharanagouda <Sharanu.mdl@gmail.com>
2019-12-09 16:35:08 +0530

    redux connected

---
+++ src/config/Routes.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route,Switch, Link } from "react-router-dom";
+
+import Home from "../container/Home";
+
+class Routes extends Component {
+    render(){
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route component={PageNotFound} />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+const PageNotFound = () => (
+    <div>
+        <section className="section content has-text-centered" style={{minHeight:"750px"}}>
+        <h1 style={{textAlign:"center", color:"#8B0000"}}> #404 Page not found</h1>
+        <p className='is-size-5' style={{borderTop:'1px solid #222', paddingTop:'20px', textAlign:"center"}}>Looks like you found a page that doesn't exist!</p>
+        <p style={{textAlign:"center"}}>You can go back home by clicking <Link to='/'>here</Link>.</p>
+    </section>
+  </div>);
+
+export default Routes;
